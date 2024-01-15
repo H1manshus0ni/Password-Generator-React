110e03d3f330e27bcf2aa91936853c7c349da97c
Himanshu Soni <himanshuso912@gmail.com>
2024-01-15 22:35:08 +0530

    commit done

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*-_+=[]{}~`";
+    }
+
+    for (let i = 0; i < length; i++) {
+      let singleChar = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(singleChar);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setNumberAllowed]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-blue-500">
+        <h1 className="text-white text-center my-3">Password generator</h1>
+
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+
+          <button
+            onClick={copyPasswordToClipboard}
+            style={{
+              backgroundColor: isHovered ? "green" : "blue",
+              color: "white",
+              padding: "0.5rem 1rem",
+              transition: "background-color 0.3s",
+            }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2 ">
+          <div className="flex items-center gap-x-1 text-white ">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label> length : {length}</label>
+          </div>
+
+          <div className="flex items-center gap-x-1 text-white ">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1  text-white ">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
